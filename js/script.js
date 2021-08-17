8d46b692bb581033617ac3363158cd44c1fd352c
Matthew Payne <71348063+Matthewpco@users.noreply.github.com>
2021-08-17 10:24:17 -0600

    Update script.js
    established api calls and HTML for displaying random user objects.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,31 @@
 const randomFolks = document.querySelector(".random-peeps");
+
+
+const getData = async function () {
+    let usersRequest = await fetch("https://randomuser.me/api?results=5");
+    let data = await usersRequest.json();
+    let usersResult = data.results;
+    displayUsers(usersResult);
+}
+
+
+getData();
+
+
+const displayUsers = function (usersResult) {
+    randomFolks.innerHTML = "";
+    for (const user of usersResult) {
+        let name = user.name.first;
+        let country = user.location.country;
+        let imageUrl =  user.picture.medium;
+        console.log(`${name}${country}${imageUrl}`)
+
+        const userDiv = document.createElement("div");
+        userDiv.innerHTML = `
+        <h3>${name}</h3>
+        <p> ${country} </p>
+        <img src=${imageUrl} alt="ALT" />` 
+        randomFolks.append(userDiv)
+    };
+    
+}
